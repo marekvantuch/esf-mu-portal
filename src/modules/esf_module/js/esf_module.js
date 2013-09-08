61f5055e52f843547964852f9320f3828dbf37bc
Marek Vantuch <marek.vantuch@gmail.com>
2013-09-09 00:13:11 +0700

    Work on the module

---
+++ src/modules/esf_module/js/esf_module.js
@@ -0,0 +1,18 @@
+(function($) {
+  var Esf = Esf || {};
+
+  Drupal.behaviors.esf = {
+    attach: function(context) {
+      $('article a').each(function(index, element){
+        var protocol = Drupal.settings.esf.protocol;
+        var re = new RegExp("(" + protocol + ":\/\/).*","g")
+        if (element.href.match(re)) {
+          var url = Drupal.settings.esf.url + ':' + Drupal.settings.esf.port
+          this.href= url;
+        }
+        console.log(element.href);
+      });
+    }
+  }
+
+})(jQuery);
