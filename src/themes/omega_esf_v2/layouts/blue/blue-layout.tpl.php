6d3c0ccbb9cff431a64c089d1bfdff0c269a54bf
Marek Vantuch <marek.vantuch@gmail.com>
2014-01-25 19:05:49 +0100

    Change layout of the site

--- src/themes/omega_esf_v2/layouts/blue/blue-layout.tpl.php
+++ src/themes/omega_esf_v2/layouts/blue/blue-layout.tpl.php
@@ -11,27 +11,18 @@
         <?php if ($site_slogan): ?>
           <h2 class="site-branding__slogan"><?php print $site_slogan; ?></h2>
         <?php endif; ?>
-        <?php print render($page['branding']); ?>
       </div>
-      <?php print render($page['navigation']); ?>
       <?php print render($page['header']); ?>
     </div>
   </header>
 
-  <?php print render($page['hero']); ?>
-
-  <?php if (!empty($page['highlighted'])): ?>
-    <div class="l-highlighted-wrapper">
-      <?php print render($page['highlighted']); ?>
-    </div>
-  <?php endif; ?>
-
   <div class="l-main l-constrained">
-    <a id="main-content"></a>
+
     <?php print render($tabs); ?>
     <?php print $breadcrumb; ?>
     <?php print $messages; ?>
-    <?php print render($page['help']); ?>
+
+    <?php print render($page['sidebar']); ?>
 
     <div class="l-content" role="main">
       <?php print render($title_prefix); ?>
@@ -45,10 +36,9 @@
       <?php print render($page['content']); ?>
       <?php print $feed_icons; ?>
     </div>
-
-    <?php print render($page['sidebar_first']); ?>
-    <?php print render($page['sidebar_second']); ?>
   </div>
+
+  <?php print render($page['postscript']); ?>
 
   <footer class="l-footer-wrapper" role="contentinfo">
     <?php print render($page['footer']); ?>
