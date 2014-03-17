b18c281d719d107d0575b03319744252bbc8d95b
Marek Vantuch <marek.vantuch@gmail.com>
2014-03-17 01:39:49 +0100

    Implement the front page dynamic hover styling function.

--- src/modules/esf_module/js/home.js
+++ src/modules/esf_module/js/home.js
@@ -1,3 +1,21 @@
 /**
  * Created by mvantuch on 1/30/14.
  */
+
+(function ($) {
+
+    Drupal.behaviors.esf_home = {
+        attach: function (context) {
+            $('.map').maphilight();
+        }
+    }
+
+    $("#quicktabs-navigation .field-content a").mouseover(function(e) {
+        $("#map-hospodarska-soutez").mouseover();
+        $("#map-dusevni-vlastnictvi").mouseover();
+    }).mouseout(function(e) {
+        $("#map-hospodarska-soutez").mouseout();
+        $("#map-dusevni-vlastnictvi").mouseout();
+    })
+
+})(jQuery);
