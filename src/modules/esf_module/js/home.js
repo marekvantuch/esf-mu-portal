1abc26f922634a41bc15ec2866a8fe631631507e
Marek Vantuch <marek.vantuch@gmail.com>
2014-03-24 01:51:06 +0100

    Add mapping for the front page layout.

--- src/modules/esf_module/js/home.js
+++ src/modules/esf_module/js/home.js
@@ -10,12 +10,43 @@
         }
     }
 
+
     $("#quicktabs-navigation .field-content a").mouseover(function(e) {
-        $("#map-hospodarska-soutez").mouseover();
-        $("#map-dusevni-vlastnictvi").mouseover();
+        var nid = get_nid($(this).parent());
+        var subjects = get_related_subjects(nid);
+
+        for (var key in subjects) {
+            if (subjects.hasOwnProperty(key)) {
+                $("#map-" + subjects[key]).mouseover();
+            }
+        }
     }).mouseout(function(e) {
-        $("#map-hospodarska-soutez").mouseout();
-        $("#map-dusevni-vlastnictvi").mouseout();
+        var nid = get_nid($(this).parent());
+        var subjects = get_related_subjects(nid);
+
+        for (var key in subjects) {
+            if (subjects.hasOwnProperty(key)) {
+                $("#map-" + subjects[key]).mouseout();
+            }
+        }
     })
+
+    function get_related_subjects(nid) {
+        return Drupal.settings.subject_map[nid];
+    }
+
+    function get_nid(object) {
+        var classes = object.attr("class").split(/\s+/);
+
+        for (var key in classes) {
+            if (classes.hasOwnProperty(key) && isNumber(classes[key])) {
+                return classes[key];
+            }
+        }
+    }
+
+    function isNumber(n) {
+        return !isNaN(parseFloat(n)) && isFinite(n);
+    }
 
 })(jQuery);
