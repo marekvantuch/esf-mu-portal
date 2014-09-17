4ca9d7ef598ba114e972bdfe4d9574183d2a35f6
Marek Vantuch <marek.vantuch@gmail.com>
2014-09-17 11:53:33 +0200

    Implement global & per-user guacamole settings

--- src/modules/esf_module/js/esf-common.js
+++ src/modules/esf_module/js/esf-common.js
@@ -3,19 +3,23 @@
 
     Drupal.behaviors.esf_common = {
         attach: function (context) {
-            $('article a').each(function (index, element) {
-                var protocol = Drupal.settings.esf.protocol;
-                var re = new RegExp("(" + protocol + ":\/\/)(.*)", "g");
-                if (element.href.match(re)) {
-                    var res = re.exec(element.href);
-                    this.href = Drupal.settings.basePath + '?q=aspi';
-                    // append the rest of ASPI connection string
-                    if (res[2]) {
-                        this.href += '&' + res[2];
+            var is_enabled = Drupal.settings.esf.is_guacamole_enabled;
+
+            if (is_enabled) {
+                $('article a').each(function (index, element) {
+                    var protocol = Drupal.settings.esf.protocol;
+                    var re = new RegExp("(" + protocol + ":\/\/)(.*)", "g");
+                    if (element.href.match(re)) {
+                        var res = re.exec(element.href);
+                        this.href = Drupal.settings.basePath + '?q=aspi';
+                        // append the rest of ASPI connection string
+                        if (res[2]) {
+                            this.href += '&' + res[2];
+                        }
                     }
-                }
-                //console.log(element.href);
-            });
+                    //console.log(element.href);
+                });
+            }
         }
     };
 
