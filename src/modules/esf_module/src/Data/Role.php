6bf2ddaa21800920179f38d02e6bd351cf6d384e
Marek Vantuch <marek.vantuch@gmail.com>
2014-11-22 14:59:06 +0100

    Update features & initial changes

---
+++ src/modules/esf_module/src/Data/Role.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Marek Vantuch
+ * Date: 11/22/2014
+ * Time: 10:08 AM
+ */
+
+namespace Drupal\esf\Data;
+
+
+class Role {
+  const MODERATOR = 'moderator';
+}
---
+++ src/modules/esf_module/src/Data/Route.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Marek Vantuch
+ * Date: 11/22/2014
+ * Time: 10:16 AM
+ */
+
+namespace Drupal\esf\Data;
+
+
+class Route {
+  const HOME = 'home';
+  const WORKBENCH = 'admin/workbench';
+  const ASPI = 'aspi';
+  const ASPI_AJAX_LOGIN = 'aspi/ajax/login';
+} 
